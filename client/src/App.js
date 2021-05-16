9651bee5f9ea5655b7d15ed6bbe4f3a47356c49c
Christopher Smith <christopher.s.smith@stonybrook.edu>
2021-05-16 14:07:03 -0400

    tried new login and failed

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,23 @@ import Login 						from './components/Login';
 import Home							from './components/Home';
 import RegionSpreadSheet			from './components/RegionSpreadSheet';
 import RegionViewer					from './components/RegionViewer';
-import { useQuery } 				from '@apollo/client';
+import { useApolloClient, 
+		 useMutation,
+		 useQuery } 				from '@apollo/client';
 import * as queries 				from './cache/queries';
+import { LOGOUT }					from './cache/mutations';
 import { jsTPS } 					from './utils/jsTPS';
 import { BrowserRouter, Switch,
+		 useHistory,
 		 Route, Redirect } 			from 'react-router-dom';
  
 const App = () => {
+
+	const [Logout] = useMutation(LOGOUT);
+
+	const history = useHistory();
+	const client = useApolloClient();
+
 	let user = null;
     let sheetTps = new jsTPS();
 	let viewerTps = new jsTPS();
@@ -24,6 +34,18 @@ const App = () => {
 	if(data) { 
 		let { getCurrentUser } = data;
 		if(getCurrentUser !== null) { user = getCurrentUser; }
+    }
+
+	const logout = async (e) => {
+        await Logout();
+        const { _, error, data } = await refetch();
+        if (error) {console.log(error.message)};
+        if (data) {
+            let reset = client.resetStore(); // store reset while query was in flight?
+            if (reset) history.push('/');
+        } else {
+            history.push('/');
+        }
     }
 
 	return(
@@ -40,16 +62,16 @@ const App = () => {
 					<Login fetchUser={refetch} />
 				</Route>
 				<Route path='/update'>
-					<UpdateAccount fetchUser={refetch} user={user} />
+					<UpdateAccount fetchUser={refetch} user={user} logout={logout} />
 				</Route>
 				<Route exact path='/home'>
-					<Home fetchUser={refetch} user={user} />
+					<Home fetchUser={refetch} user={user} logout={logout} />
 				</Route>
 				<Route path='/home/sheet/:id'>
-                    <RegionSpreadSheet fetchUser={refetch} user={user} sheetTps={sheetTps} />
+                    <RegionSpreadSheet fetchUser={refetch} user={user} sheetTps={sheetTps} logout={logout} />
                 </Route>
 				<Route path='/home/view/:id'>
-					<RegionViewer fetchuser={refetch} user={user} viewerTps={viewerTps} />
+					<RegionViewer fetchuser={refetch} user={user} viewerTps={viewerTps} logout={logout} />
 				</Route>
 			</Switch>
 		</BrowserRouter>
